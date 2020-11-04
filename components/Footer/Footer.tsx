b672becc96568d8ffc4c0d1d67398831a93d7002
Bartłomiej <bartekfc17@gmail.com>
2020-11-04 13:07:28 +0100

    Changes to Footer and deleted .idea folder

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -9,16 +9,16 @@ export default function Footer() {
       <Container>
         <div className={styles.wrapper}>
           <Newsletter />
-          {/*<section id="about">*/}
-          {/*  <h2 className={styles.heading}>About</h2>*/}
-          {/*  <h3 className={styles.greetingHeading}>Hi I'm Bartek</h3>*/}
-          {/*  <p className={styles.description}>*/}
-          {/*    Hi I am passionate of web development since last year. I am high*/}
-          {/*    school student from Poland. I am creating my websites using React*/}
-          {/*    and Typescript. I hope you will find interesting content on my*/}
-          {/*    blog ;).*/}
-          {/*  </p>*/}
-          {/*</section>*/}
+          <section id='about'>
+            <h2 className={styles.heading}>About</h2>
+            <h3 className={styles.greetingHeading}>Hi I'm Bartek</h3>
+            <p className={styles.description}>
+              Hi I am passionate of web development since last year. I am high
+              school student from Poland. I am creating my websites using React
+              and Typescript. I hope you will find interesting content on my
+              blog ;).
+            </p>
+          </section>
         </div>
       </Container>
     </footer>
